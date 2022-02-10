e45a805699689114e1a1622f3f027ee3a6a0fcb3
Luis Ramos <luismiguel.ramos@yahoo.com.au>
2022-02-11 03:28:22 +1100

    made a constants file

---
+++ src/components/Constants.js
@@ -0,0 +1,106 @@
+
+const sites = [
+  {
+    id: 0,
+    title: 'Argentinian League',
+    source: 'https://www.afa.com.ar/es/'
+  },
+  { 
+    id: 1,
+    title: 'Australian League',
+    source: 'https://www.ultimatealeague.com/'
+  },
+  {    
+    id: 2,
+    title: 'Brazilian League',
+    source: 'https://www.espn.com.au/football/league/_/name/bra.1'
+  },
+  {    
+    id: 3,
+    title: 'Chinese League',
+    source: 'https://www.thecfa.cn/Eindex/index.html'
+  },
+  {  
+    id: 4,
+    title: 'Dutch League',
+    source: 'https://eredivisie.eu/home/'
+  },
+  {    
+    id: 5,
+    title: 'English League',
+    source: 'https://www.premierleague.com/'
+  },
+  {    
+    id: 6,
+    title: 'French League',
+    source: 'https://www.ligue1.com/'
+  },
+  {    
+    id: 7,
+    title: 'German League',
+    source: 'https://www.bundesliga.com/en/bundesliga'
+  },
+  {    
+    id: 8,
+    title: 'Indonesian League',
+    source: 'https://www.espn.com.au/football/league/_/name/idn.1'
+  },
+  {    
+    id: 9,
+    title: 'Italian League',
+    source: 'https://www.legaseriea.it/en'
+  },
+  {
+    id: 10,
+    title: 'Japanese League',
+    source: 'https://www.jleague.co/'
+  },
+  {
+    id: 11,
+    title: 'Malaysian League',
+    source: 'https://www.malaysianfootballleague.com/'
+  },
+  {
+    id: 12,
+    title: 'Mexican League',
+    source: 'https://www.espn.com.au/football/schedule/_/league/mex.1'
+  },
+  {
+    id: 13,
+    title: 'Portuguese League',
+    source: 'https://www.ligaportugal.pt/en/homepage/'
+  },
+  {
+    id: 14,
+    title: 'Russian League',
+    source: 'https://eng.premierliga.ru/'
+  },
+  {
+    id: 15,
+    title: 'Singaporean League',
+    source: 'https://www.spl.sg/'
+  },
+  {
+    id: 16,
+    title: 'Spanish League',
+    source: 'https://www.laliga.com/en-GB'
+  },
+  {
+    id: 17,
+    title: 'Thai League',
+    source: 'https://www.thaileague.co.th/131'
+  },
+  {
+    id: 18,
+    title: 'Turkish League',
+    source: 'https://www.tff.org/Default.aspx?pageID=449'
+  },
+  {
+    id: 19,
+    title: 'Ugandan League',
+    source: 'https://fufa.co.ug/uganda-premier-league/'
+  },
+
+]
+
+export default sites
