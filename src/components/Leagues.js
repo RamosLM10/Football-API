5c86cebcc935966f39ae9b70968babfcf61d5825
Luis Ramos <luismiguel.ramos@yahoo.com.au>
2022-02-11 03:31:42 +1100

    added links to images for leagues

--- src/components/Leagues.js
+++ src/components/Leagues.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../App.css';
 import axios from 'axios';
+import  sites  from './Constants';
 
 const Leagues = () => {
   const [data, setData] = useState([]);
@@ -14,14 +15,35 @@ const Leagues = () => {
     })
   }, [])
 
-  return <div className='leagues-container'>
-    {data.map((data)=> (
-      <div key={data.id} className='leagues-div'>
-        <img src={data.logos.light}/>
-        <h1>{data.name}</h1>
-      </div>
-    ))}
-  </div>;
+  const findSource =(index) => {
+    const site = sites.find((el) => el.id === index)
+    return site.source;
+  }
+
+  return (
+    <div className='leagues-container'>
+    {data.map((data,index)=> (
+          <div key={data.id}>
+            <a href={findSource(index)} target='_blank' rel='noreferrer' className='leagues-div'>
+              <img src={data.logos.light} alt={data.name} />
+            </a>
+            <h1>{data.name}</h1>
+          </div>
+       ))}
+    </div>
+  );
+
+  // return <div className='leagues-container'>
+  //   {data.map((dataObj)=> (sites.filter(s => s.id === dataObj.id).map(({ source })=> ( (
+  //         <div key={data.id}>
+  //           <a href={source} target='_blank' rel='noreferrer' className='leagues-div'>
+  //             <img src={data.logos.light} />
+  //           </a>
+  //           <h1>{data.name}</h1>
+  //         </div>
+  //       )))))}
+  //   </div>;
 };
+  
 
 export default Leagues;
